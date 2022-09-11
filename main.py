2a50eba7726c1eba0628127af11575233b9bb636
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-11 16:44:43 +0200

    Add Styling

--- main.py
+++ main.py
@@ -5,50 +5,69 @@ BUTTON_HEIGHT = 17.5
 
 RESULT = ""
 
+BUTTON_COLOR = "#1c2e4a"
+TEXT_COLOR = "#FAF9F9"
+CLEAR_BUTTON_COLOR ="#F96900"
+BG_COLOR = "#152238"
+EQUAL_COLOR = "#CA2E55"
+
+
 class Button:
     def __init__(self, number = "1", x_grid = 1, y_grid = 1, span = 1) -> None:
         self.number = number
         
         if self.number == "=":
             command = self.evaluate
+            button_color = EQUAL_COLOR
         elif self.number == "C":
             command = self.clear
+            button_color = CLEAR_BUTTON_COLOR
         else:
             command = self.func
+            button_color = BUTTON_COLOR
         
-        self.button = tk.Button( 
-            frame_button,
+        self.button = tk.Button(
+            frame_button, 
             text = number,
             font = ("Consolas", 14),
             padx=BUTTON_WIDTH,
             pady=BUTTON_HEIGHT,
+            bg = button_color,
+            fg = TEXT_COLOR,
+            bd = 0,
             command=command
             )
         self.button.grid(row=x_grid, column=y_grid, columnspan=span, padx = 1, pady = 1)
-    
+        
     def func(self):
         global RESULT
         RESULT += self.number
         display.display_config()
         
-    def clear(self):
-        display.display_clear()
-        
     def evaluate(self):
         global RESULT
         RESULT = str(eval(RESULT))
         display.display_config()
-
+        
+    def clear(self):
+        display.display_clear()
+            
+        
+        
+        
 class Display:
     def __init__(self) -> None:
         self.text_box = tk.Text(
-            dispaly_frame,
+            dispaly_frame, 
             font = ("Consolas", 24),
             width=1,
             height = 2,
+            bg = BG_COLOR,
+            fg = TEXT_COLOR,
+            bd = 0,
             )
         self.text_box.pack(fill=tk.BOTH)
-        
+    
     def display_config(self):
         global RESULT
         self.text_box.delete(1.0, "end")
@@ -59,17 +78,19 @@ class Display:
         RESULT = ""
         self.text_box.delete(1.0, "end")
         
-        
+    
+
+
 win = tk.Tk()
 win.title("Calculator")
 win.iconbitmap("imgs/icon.ico")
 
-dispaly_frame = tk.LabelFrame(win)
+dispaly_frame = tk.LabelFrame(win, bg = BG_COLOR)
 dispaly_frame.pack(fill=tk.BOTH)
 
 display = Display()
 
-frame_button = tk.LabelFrame(win)
+frame_button = tk.LabelFrame(win, bg = BG_COLOR)
 frame_button.pack(fill=tk.BOTH)
 
 button1 = Button("1", 3, 1)
@@ -93,9 +114,9 @@ minus_button = Button("-", 2, 4)
 mult_button = Button("*", 3, 4)
 divide_button = Button("/", 4, 4)
 
+
 equal_button = Button("=", 5,1, 2)
 bracket_left_button = Button("(", 5,3)
 bracket_right_button = Button(")", 5,4)
-
 
 win.mainloop()
